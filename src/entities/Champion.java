80b3deaa468d8bfa245395c734a218f424f8ace5
Virgilio Sirqueira Cruvinel <virgiliocomp@gmail.com>
2025-05-27 19:07:58 -0300

    Creation of the Champion class

---
+++ src/entities/Champion.java
@@ -0,0 +1,72 @@
+package entities;
+
+public class Champion   {
+
+    private String name;
+    private int life;
+    private int attack;
+    private int armor;
+
+    public Champion(String name, int life, int attack, int armor) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+        this.armor = armor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getLife() {
+        return life;
+    }
+
+    public void setLife(int life) {
+        this.life = life;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+
+    public int getArmor() {
+        return armor;
+    }
+
+
+    public void takeDamage(Champion other){
+        int otherAttack;
+        int damageTaken;
+        int currentHealth;
+
+        otherAttack = other.getAttack();
+        damageTaken = otherAttack - getArmor();
+
+        if (damageTaken <= 0){
+            damageTaken = 1;
+        }
+
+
+        currentHealth = getLife() - damageTaken;
+
+        if (currentHealth < 0){
+            setLife(0);
+        } else {
+            setLife(currentHealth);
+        }
+
+        status();
+    }
+
+    public String status(){
+        if (getLife() <= 0){
+            setLife(0);
+            return getName() + ": " + getLife() + " de vida (morreu)";
+        }
+        return getName() + ": " + getLife() + " de vida";
+    }
+
+}
